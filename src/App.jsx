c1fd7b7e3b05b35b65fbf69ac58513a0cc23bae8
Murilo Dias de Oliveira <murilodiasdeoliveira2022@gmail.com>
2025-10-14 21:50:23 -0300

    header estilizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
-
-
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Components/Login/Login';
+import './App.css'
 
 const App = () => {
   return (
     <div>
-      <h1>React</h1>
+      <BrowserRouter>
+        <Header />
+        <Routes> 
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 };
